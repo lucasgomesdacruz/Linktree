4546eca20d89c525d46a03de8ca4f28a4c9aecd6
Lucas Gomes <lucaass1997rp@gmail.com>
2024-12-15 20:11:08 -0300

    :tada: feat: versão inicial linktree

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Button from './components/Button/Button';
+
+function App() {
+  
+  return (
+    <main className="backround">
+      <div className="dark">
+        <h1>Lucas Desenvolvedor</h1>
+        <Button href="https://zaion1997.vercel.app/" title="Portfólio" />
+        <Button href="https://github.com/lucasgomesdacruz" title="GitHub" />
+        <Button href="https://www.linkedin.com/in/lucaass1997/" title="Linkedin" />
+      </div>
+    </main>
+  )
+  
+}
+export default App
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import "./Button.scss"
+
+interface Props {
+    title: string;
+    href: string;
+}
+
+function Button({title , href}: Props) {
+    return (
+        <a href={href} target="blank">
+            <article>
+                <h2>{title}</h2>
+            </article>
+        </a>
+    )
+}
+
+export default Button
